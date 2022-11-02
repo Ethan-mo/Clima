ca9a060a5eb29d5915dd0bdc1d20463fc0ec25ef
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2022-11-02 20:44:25 +0900

    ✨[feat] 1. light모드, dark모드 설정(assets)

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        // 이제 textField에서 작업이 끝난것으로 보인다면..
+        if searchTextField.text != ""{
+            // 키보드를 내린다(끝 처리를 해준다.)
+            return true
+        }else{
+            textField.placeholder = "도시를 입력해주세요."
+            // 키보드를 내리지 않는다.(끝 처리를 해주지 않는다.)
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text == ""
+    }
+    //My API Key: 2a190fbf7dce2c5d1ef8c1d7fe8906b1
 }
 
